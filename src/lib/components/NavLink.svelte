d78fdd42c649f320ed0431d0ef142f8ca52307f8
Mateo Fuzul <mfuzul@hotmail.com>
2023-01-16 22:12:42 +0100

    feat(NavLink): add btn-active class

--- src/lib/components/NavLink.svelte
+++ src/lib/components/NavLink.svelte
@@ -1,7 +1,15 @@
 <script>
+    import {onMount} from 'svelte';
+
+    let currentPath;
+    
+    onMount(() => {
+        currentPath = window.location.pathname;
+    });
+
     export let href = '';
     export let content = '';
     export let variant = 'primary';
 </script>
 
-<a {href} class="btn btn-{variant}">{content}</a>
+<a {href} class="btn btn-{variant}" class:btn-active={currentPath === href}>{content}</a>
