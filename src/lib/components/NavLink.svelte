45466b1ac960b27c0269936e60de5f5e10deaefd
Mateo Fuzul <mfuzul@hotmail.com>
2023-01-16 22:08:51 +0100

    feat(navbar): add table and map view

---
+++ src/lib/components/NavLink.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let href = '';
+    export let content = '';
+    export let variant = 'primary';
+</script>
+
+<a {href} class="btn btn-{variant}">{content}</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,18 @@
 <script>
 	import '$lib/global.css';
 	import Icon from '$lib/components/Icon.svelte';
+    import NavLink from '$lib/components/NavLink.svelte';
 </script>
 
 <div class="navbar bg-base-100">
+    <div class="flex-initial">
 	<Icon />
+    </div>
+    <div class="flex-initial ml-4">
+        <ul class="menu menu-compact menu-horizontal px-1">
+          <li class="mr-4"><NavLink href={'/'} content={'Table view'}/></li>
+          <li><NavLink href={'/map'} content={'Map view (coming soon)'} variant={'disabled'}/></li>
+        </ul>
+      </div>
 </div>
 <slot />
