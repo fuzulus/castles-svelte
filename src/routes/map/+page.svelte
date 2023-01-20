172600dfe2f1dfeac9e8328a362b64975300614c
Mateo Fuzul <mfuzul@hotmail.com>
2023-01-20 08:14:04 +0100

    fix(map): initialize map in browser only

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,23 +1,26 @@
 <script>
     import {onMount} from 'svelte';
+    import { browser } from '$app/environment'
     import L from 'leaflet';
 
     /** @type {import('./$types').PageData} */
 	export let data;
     
     onMount(() => {
-        const map = L.map('map').setView([44.8401746, 15.8942923], 7);
+        if (true === browser) {
+            const map = L.map('map').setView([44.8401746, 15.8942923], 7);
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
 
-        for (const castle of data.castles) {
-            L
-                .marker([castle.lat, castle.long])
-                .bindPopup(castle.name)
-                .addTo(map);
+            for (const castle of data.castles) {
+                L
+                    .marker([castle.lat, castle.long])
+                    .bindPopup(castle.name)
+                    .addTo(map);
+            }
         }
     });
 </script>
