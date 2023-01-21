5509748a21067482d8f1bea65b8654585e699c35
Mateo Fuzul <mfuzul@hotmail.com>
2023-01-21 01:05:41 +0100

    feat(pagination): add Paginator class

---
+++ src/lib/services/pagination/Paginator.ts
@@ -0,0 +1,37 @@
+export default class Paginator {
+    private static pages: (number|null)[] = [];
+
+    public static generatePages(
+        currentPage: number,
+        pageCount: number,
+        pageStep: number,
+    ): (number|null)[] {
+        Paginator.pages = [];
+        
+        if (pageCount < pageStep * 2 + 6) {
+            Paginator.addPages(1, pageCount + 1);
+		} else if (currentPage < pageStep * 2 + 1) {
+            Paginator.addPages(1, pageStep * 2 + 4);
+            Paginator.pages.push(null);
+			Paginator.pages.push(pageCount);
+		} else if (currentPage > pageCount - pageStep * 2) {
+			Paginator.pages.push(1);
+            Paginator.pages.push(null);
+            Paginator.addPages(pageCount - pageStep * 2 - 2, pageCount + 1);
+		} else {
+			Paginator.pages.push(1);
+            Paginator.pages.push(null);
+            Paginator.addPages(currentPage - pageStep, currentPage + pageStep + 1);
+            Paginator.pages.push(null);
+			Paginator.pages.push(pageCount);
+		}
+
+        return Paginator.pages;
+    }
+
+    private static addPages(i: number, j: number) {
+        for (let page = i; page < j; page++) {
+            Paginator.pages.push(page);
+        }
+    }
+}
